08c347a28112442db3fffd3132696b209bfb3df2
andevew <31newbie.2009@gmail.com>
2025-05-11 19:06:14 +0200

    some styles

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -1,19 +1,33 @@
 import './Main.scss';
 
 function Main() {
-    return (  
+    return (
         <div className='container'>
-            <h1>Your notes, locked and loaded</h1>
+            <h1 className='h1'>Your notes, locked and loaded</h1>
 
-            <h2>Fast. Secure. Yours</h2>
+            <h3 className='h3'>Fast. Secure. Yours</h3>
 
             <button>Start Writing</button>
 
             <div className="properties">
-                <div className="card">Secure</div>
-                <div className="card">Fast</div>
-                <div className="card">Cross-platform</div>
+                <div className="card">
+                    <h4 className='card-heading h4'>
+                        Secure
+                    </h4>
+                </div>
+                <div className="card">
+                    <h4 className='card-heading h4'>
+                        Fast
+                    </h4>
+                </div>
+                <div className="card">
+                    <h4 className='card-heading h4'>
+                        Cross-platform
+                    </h4>
+                </div>
             </div>
+
+            <a href="#">Some link</a>
 
         </div>
     );
